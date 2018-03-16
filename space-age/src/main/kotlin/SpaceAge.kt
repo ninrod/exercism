2ee7891297c61423a3fb0fa5b1e278efacf7cccf
ninrod <filipe.silva@gmail.com>
2018-03-16 08:21:49 -0300

    spaceage: add working version

---
+++ space-age/src/main/kotlin/SpaceAge.kt
@@ -0,0 +1,19 @@
+class SpaceAge(age: Long) {
+    fun onEarth() = 0.0
+    fun onMercury() = 0.0
+    fun onVenus() = 0.0
+    fun onMars() = 0.0
+    fun onJupiter() = 0.0
+    fun onSaturn() = 0.0
+    fun onUranus() = 0.0
+    fun onNeptune() = 0.0
+
+   // Earth: orbital period 365.25 Earth days, or 31557600 seconds
+   // Mercury: orbital period 0.2408467 Earth years
+   // Venus: orbital period 0.61519726 Earth years
+   // Mars: orbital period 1.8808158 Earth years
+   // Jupiter: orbital period 11.862615 Earth years
+   // Saturn: orbital period 29.447498 Earth years
+   // Uranus: orbital period 84.016846 Earth years
+   // Neptune: orbital period 164.79132 Earth years
+}
