24b27c9aba7e606e932637d4ae2fb3ef601a4f97
ninrod <filipe.silva@gmail.com>
2018-04-07 00:06:54 -0300

    add makefile

--- acronym/src/main/kotlin/Acronym.kt
+++ acronym/src/main/kotlin/Acronym.kt
@@ -1,3 +1,14 @@
 object Acronym {
-   fun generate(f:String) = f.split(' ','-').map{it[0].toUpperCase()}.joinToString("")
+   fun generate(f: String) = f.split(' ', '-').map{ it[0].toUpperCase() }.joinToString("")
+}
+
+fun main(args: Array<String>) {
+    val map = mapOf(
+            1 to "Filipe",
+            2 to "Evelize",
+            3 to "Juliana",
+            4 to "Adelva",
+            5 to "Caim"
+    )
+    println(map)
 }
