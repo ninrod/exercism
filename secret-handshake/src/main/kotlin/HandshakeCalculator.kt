f5ae2ebd049cc3eeffc848de80781efc8a7715d1
ninrod <filipe.silva@gmail.com>
2018-04-11 14:44:10 -0300

    add secret handshake bitwise fumblings

--- secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -3,3 +3,19 @@ object HandshakeCalculator {
         return listOf(Signal.CLOSE_YOUR_EYES)
     }
 }
+
+fun main(args: Array<String>) {
+    val n = 3
+    val r = n and 0b11111
+    val x = r and 0b10000
+    println(r.toString(2))
+    println(x.toString(2))
+}
+
+/*
+00001 - wink
+00010 - double blink
+00100 - close your eyes
+01000 - jump
+10000 - reverse
+*/
--- secret-handshake/src/main/kotlin/Signal.kt
+++ secret-handshake/src/main/kotlin/Signal.kt
@@ -1,5 +1,7 @@
-enum class Signal {
-
-  WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
-
+enum class Signal(bit: Int) {
+  WINK(0x01),
+  DOUBLE_BLINK(0x02),
+  CLOSE_YOUR_EYES(0x04),
+  JUMP(0x08),
+  REVERSE(0x10)
 }
