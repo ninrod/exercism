77d35dd0af00bc4e75dee7bff108fdebfb9d4554
ninrod <filipe.silva@gmail.com>
2018-04-11 13:36:25 -0300

    diffsquares: make Int.square private

--- difference-of-squares/src/main/kotlin/Squares.kt
+++ difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,5 +1,5 @@
-inline fun Int.square() = this * this
 class Squares(private val number: Int) {
+    private inline fun Int.square() = this * this
     fun squareOfSum() = (1..number).sum().square()
     fun sumOfSquares() = (1..number).map { it.square() }.sum()
     fun difference() = squareOfSum() - sumOfSquares()
