e32d579ac7e837f57710a2f7da1523bd3ea0f144
ninrod <filipe.silva@gmail.com>
2018-04-11 11:36:04 -0300

    difference of squares: add failing testes

---
+++ difference-of-squares/src/main/kotlin/Squares.kt
@@ -0,0 +1,11 @@
+class Squares(val number: Int) {
+    fun squareOfSum(): Int {
+        return number
+    }
+    fun sumOfSquares(): Int {
+        return number
+    }
+    fun difference(): Int {
+        return number
+    }
+}
