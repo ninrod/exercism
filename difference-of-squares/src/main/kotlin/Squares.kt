29985e0bde139e6672fc31a8b7daa11a5947b03f
ninrod <filipe.silva@gmail.com>
2018-04-11 13:43:44 -0300

    squares: further optimization

--- difference-of-squares/src/main/kotlin/Squares.kt
+++ difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,6 +1,6 @@
+private inline fun Int.square() = this * this
 class Squares(private val number: Int) {
-    private inline fun Int.square() = this * this
     fun squareOfSum() = (1..number).sum().square()
-    fun sumOfSquares() = (1..number).map { it.square() }.sum()
+    fun sumOfSquares() = (1..number).sumBy(Int::square)
     fun difference() = squareOfSum() - sumOfSquares()
 }
