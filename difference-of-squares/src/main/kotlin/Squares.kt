b85d24f186d464f31f1e7c8d39bf0451a0bf01b2
ninrod <filipe.silva@gmail.com>
2018-04-11 12:07:48 -0300

    resolve difference of squares

--- difference-of-squares/src/main/kotlin/Squares.kt
+++ difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,11 +1,6 @@
-class Squares(val number: Int) {
-    fun squareOfSum(): Int {
-        return number
-    }
-    fun sumOfSquares(): Int {
-        return number
-    }
-    fun difference(): Int {
-        return number
-    }
+inline fun Int.square(): Int = this * this
+class Squares(private val number: Int) {
+    fun squareOfSum(): Int = (1..number).sum().square()
+    fun sumOfSquares(): Int = (1..number).map { it.square() }.sum()
+    fun difference(): Int = squareOfSum() - sumOfSquares()
 }
