dff9cddd192d05747cbd1b33b923e55df48bfb25
ninrod <filipe.silva@gmail.com>
2018-04-11 23:50:03 -0300

    perfect-numbers: oneliner

--- perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -1,14 +1,12 @@
 enum class Classification { DEFICIENT, PERFECT, ABUNDANT }
-private fun Int.aliquotSum() = (this/2 downTo 1).filter { this % it == 0 }.sum()
-private fun Int.aliquotClassify(): Classification {
-    val s = this.aliquotSum()
-    return when {
-        s > this -> Classification.ABUNDANT
-        s < this -> Classification.DEFICIENT
-        else ->  Classification.PERFECT
-    }
-}
-fun classify(n: Int): Classification {
-    require(n > 0) { "natural numbers are greater than zero" }
-    return n.aliquotClassify()
-}
+fun classify(n: Int): Classification =
+        n.let {
+            require(it > 0) { "$it should be > 0" }
+            (it/2 downTo 1).filter { n % it == 0 }.sum()
+        }.compareTo(n).let {
+            when {
+                it > 0 -> Classification.ABUNDANT
+                it < 0 -> Classification.DEFICIENT
+                else -> Classification.PERFECT
+            }
+        }
