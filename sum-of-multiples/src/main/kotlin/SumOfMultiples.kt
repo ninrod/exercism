6a1b9f87a3767ceec9e3f2739723008bd2416d40
ninrod <filipe.silva@gmail.com>
2018-04-13 10:14:31 -0300

    sum-of-multiples: remove main

--- sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
+++ sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
@@ -1,8 +1,3 @@
 object SumOfMultiples {
     fun sum(s: Set<Int>, n: Int):Int = ((s.min() ?: 0) until n).filter { s.any { f -> it % f == 0 } }.sum()
 }
-
-fun main(args: Array<String>) {
-    val s = SumOfMultiples.sum(setOf(30,50,70,110),200)
-    println(s)
-}
