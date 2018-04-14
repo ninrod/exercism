06a2f6e27765eeeaedd6c9cd4e06e422b11448ff
ninrod <filipe.silva@gmail.com>
2018-04-14 07:52:50 -0300

    luhn: remove redundant let call

--- luhn/src/main/kotlin/Luhn.kt
+++ luhn/src/main/kotlin/Luhn.kt
@@ -1,6 +1,9 @@
+private fun Int.isDivisibleBy(i: Int) = this % i == 0
+private fun Int.isEven() = this.isDivisibleBy(2)
+private fun Char.int() = this.toString().toInt()
+private fun Int.luhn() = (this * 2).let { if (it > 9) it - 9 else it }
+
 object Luhn {
-    private fun Int.isDivisibleBy(i: Int) = this % i == 0
-    private fun Int.isEven() = this.isDivisibleBy(2)
     fun isValid(a: String): Boolean {
         val s = a.replace(" ", "")
         when {
@@ -8,13 +11,12 @@ object Luhn {
             s.length < 2 -> return false
             s.toLongOrNull() == null -> return false
         }
-        return s.reversed().mapIndexed { i, c ->
-            if (i.isEven())
-                c.toString().toInt()
-            else {
-                val v = c.toString().toInt() * 2
-                if (v > 9) v - 9 else v
-            }
-        }.sum().isDivisibleBy(10)
+        return s.reversed()
+                .mapIndexed { i, c -> if (i.isEven()) c.int() else c.int().luhn() }
+                .sum().isDivisibleBy(10)
     }
+}
+
+fun main(args: Array<String>) {
+    println(6.luhn())
 }
