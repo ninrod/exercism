c94d27fb5fbc746a320bc00325cda3378e4413a0
ninrod <filipe.silva@gmail.com>
2018-04-14 09:59:04 -0300

    triangle: add initial solution

--- triangle/src/main/kotlin/Triangle.kt
+++ triangle/src/main/kotlin/Triangle.kt
@@ -1,16 +1,13 @@
-class Triangle() {
-    constructor(a: Number, b: Number, c : Number): this() {
-        require(a.toDouble() > 0 && b.toDouble() > 0 && c.toDouble() > 0) {
-            "all sides can't be 0 at the same time"
-        }
+class Triangle(aa: Number, bb: Number, cc: Number) {
+    private val a = aa.toDouble()
+    private val b = bb.toDouble()
+    private val c = cc.toDouble()
+    private val l = listOf(a,b,c).sorted()
+    init {
+        require(l.sum() > 0) { "sum of sides must be > 0" }
+        require(l[0] + l[1] > l[2])
     }
-    val isEquilateral: Boolean get() {
-        return false
-    }
-    val isIsosceles: Boolean get() {
-        return false
-    }
-    val isScalene: Boolean get() {
-        return false
-    }
+    val isEquilateral: Boolean get() = a == b && a == c
+    val isIsosceles: Boolean get() = a == b || a == c || b == c
+    val isScalene: Boolean get() = a != b && a != c && b != c
 }
