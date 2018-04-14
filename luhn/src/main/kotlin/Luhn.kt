b15e55ed7fa461adb51d9b752e3a26104fc252b0
ninrod <filipe.silva@gmail.com>
2018-04-14 07:55:11 -0300

    luhn: refine inline method usage

--- luhn/src/main/kotlin/Luhn.kt
+++ luhn/src/main/kotlin/Luhn.kt
@@ -1,8 +1,6 @@
 private fun Int.isDivisibleBy(i: Int) = this % i == 0
-private fun Int.isEven() = this.isDivisibleBy(2)
 private fun Char.int() = this.toString().toInt()
-private fun Int.luhn() = (this * 2).let { if (it > 9) it - 9 else it }
-
+private fun Char.luhn() = (this.int() * 2).let { if (it > 9) it - 9 else it }
 object Luhn {
     fun isValid(a: String): Boolean {
         val s = a.replace(" ", "")
@@ -12,11 +10,7 @@ object Luhn {
             s.toLongOrNull() == null -> return false
         }
         return s.reversed()
-                .mapIndexed { i, c -> if (i.isEven()) c.int() else c.int().luhn() }
+                .mapIndexed { i, c -> if (i.isDivisibleBy(2)) c.int() else c.luhn() }
                 .sum().isDivisibleBy(10)
     }
-}
-
-fun main(args: Array<String>) {
-    println(6.luhn())
 }
