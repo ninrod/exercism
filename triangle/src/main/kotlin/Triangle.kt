898b6634d3bb8e9e6966422593c0df0a28761961
ninrod <filipe.silva@gmail.com>
2018-04-14 10:40:09 -0300

    triangle: refine solution

--- triangle/src/main/kotlin/Triangle.kt
+++ triangle/src/main/kotlin/Triangle.kt
@@ -1,13 +1,10 @@
-class Triangle(aa: Number, bb: Number, cc: Number) {
-    private val a = aa.toDouble()
-    private val b = bb.toDouble()
-    private val c = cc.toDouble()
-    private val l = listOf(a,b,c).sorted()
+class Triangle(a: Number, b: Number, c: Number) {
+    private val l = listOf(a.toDouble(), b.toDouble(), c.toDouble()).sorted()
     init {
         require(l.sum() > 0) { "sum of sides must be > 0" }
         require(l[0] + l[1] > l[2])
     }
-    val isEquilateral: Boolean get() = a == b && a == c
-    val isIsosceles: Boolean get() = a == b || a == c || b == c
-    val isScalene: Boolean get() = a != b && a != c && b != c
+    val isEquilateral: Boolean get() = l[0] == l[1] && l[1] == l[2]
+    val isIsosceles: Boolean get() = l[0] == l[1] || l[0] == l[2] || l[1] == l[2]
+    val isScalene: Boolean get() = l[0] != l[1] && l[0] != l[2] && l[1] != l[2]
 }
