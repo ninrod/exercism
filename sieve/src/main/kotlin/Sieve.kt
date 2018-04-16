5decd1c78e39cfc7f7ecd39e1365ffbdcc16ecc4
ninrod <filipe.silva@gmail.com>
2018-04-16 14:57:51 -0300

    sieve: add list play

--- sieve/src/main/kotlin/Sieve.kt
+++ sieve/src/main/kotlin/Sieve.kt
@@ -1,3 +1,10 @@
 object Sieve {
-    fun primesUpTo(i: Int): List<Int> = listOf(1)
+    fun primesUpTo(n: Int): List<Int> {
+        return listOf(1)
+    }
+}
+fun main(args: Array<String>) {
+    val n = 10
+    val l = (2..n).mapNotNull { if (it % 2 == 0) it else null }
+    println(l)
 }
