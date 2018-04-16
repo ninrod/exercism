b12f075e701d43070730a0eea9aaf31379f82f7a
ninrod <filipe.silva@gmail.com>
2018-04-16 10:13:21 -0300

    grains: refine solution

--- grains/src/main/kotlin/Board.kt
+++ grains/src/main/kotlin/Board.kt
@@ -1,11 +1,9 @@
 import java.math.BigInteger
 
 object Board {
-    fun getGrainCountForSquare(s: Long): BigInteger {
-        require(s in (1..64)) {"Only integers between 1 and 64 (inclusive) are allowed"}
-        return BigInteger.valueOf(2).pow(s.toInt() - 1)
-    }
-    fun getTotalGrainCount(): BigInteger = (1..64).fold(BigInteger.ZERO) {
-        a, e -> a.add(getGrainCountForSquare(e.toLong()))
-    }
+    fun getGrainCountForSquare(s: Long): BigInteger =
+        require(s in (1..64)) {"Only integers between 1 and 64 (inclusive) are allowed"}.let {
+            2.toBigInteger().pow(s.toInt() - 1)
+        }
+    fun getTotalGrainCount() = (1..64).map { getGrainCountForSquare(it.toLong()) }.reduce { a, e -> a + e }
 }
