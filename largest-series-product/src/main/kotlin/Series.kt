7beaafb9b49a3693d6920e30bbe29dea617414bb
ninrod <filipe.silva@gmail.com>
2018-04-15 23:39:53 -0300

    larges-series-product: add working solution

--- largest-series-product/src/main/kotlin/Series.kt
+++ largest-series-product/src/main/kotlin/Series.kt
@@ -1,16 +1,19 @@
 private fun String.isValidNumber() = this.toBigDecimalOrNull() != null
+private fun Char.int() = this.toString().toLong()
 class Series(private val s: String) {
     init {
         if(s.isNotEmpty())
             require(s.isValidNumber()) { "string will not parse to a valid number."}
     }
-    fun getLargestProduct(n: Int): Int {
+    fun getLargestProduct(n: Int): Long {
         require(s.length >= n) { "n=$n is greater then s.length=${s.length}"}
+        require(n >= 0) { "n=$n must be positive" }
         if (n == 0) return 1
-        return 0
+        return s.mapIndexed { i, _ ->
+            if (i <= s.length - n)
+                s.substring((i until i+n)).map{ it.int() }.reduce { a, e -> a * e }
+            else
+                null
+        }.filterNotNull().max() ?: 0
     }
-}
-
-fun main(args: Array<String>) {
-
 }
