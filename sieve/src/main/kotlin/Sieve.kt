54a88dc01a9038492d4d03f44fa75ea35d2fc2ee
ninrod <filipe.silva@gmail.com>
2018-04-16 23:27:17 -0300

    sieve: remove fluff

--- sieve/src/main/kotlin/Sieve.kt
+++ sieve/src/main/kotlin/Sieve.kt
@@ -1,10 +1,4 @@
 object Sieve {
-    private fun sieve(l: List<Int>): List<Int> = if (l.isEmpty()) l
-            else l.take(1) + sieve(l.drop(1) - (l.first()..l.last()).step(l.first()).drop(1).toList())
+    private fun sieve(l: List<Int>): List<Int> = if (l.isEmpty()) l else l.take(1) + sieve(l.drop(1) - (l.first()..l.last()).step(l.first()).drop(1).toList())
     fun primesUpTo(n: Int): List<Int> = sieve((2..n).toList())
-}
-
-fun main(args: Array<String>) {
-    val l = Sieve.primesUpTo(20)
-    println(l)
 }
