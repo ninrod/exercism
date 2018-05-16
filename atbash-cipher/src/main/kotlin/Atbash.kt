43e5dbf5e721791d859584d5bade9cb4095df07d
ninrod <filipe.silva@gmail.com>
2018-05-16 10:24:29 -0300

    add passing Atbash encode function

--- atbash-cipher/src/main/kotlin/Atbash.kt
+++ atbash-cipher/src/main/kotlin/Atbash.kt
@@ -1,4 +1,21 @@
 object Atbash {
-    fun encode(input: String): String = ""
+    val alphabet = "abcdefghijklmnopqrstuvwxyz"
+    val numbers = "123456789"
+    val e = alphabet + numbers
+    val er = alphabet.reversed() + numbers
+    var map = e.mapIndexed { i, c ->  c to er[i] }.toMap()
+
+    fun encode(input: String): String =
+            input.replace("""[\s,.,${'!'},${'?'},,]""".toRegex(), "")
+                .toLowerCase()
+                .map { map[it] }.joinToString("")
+                .chunked(5)
+                .joinToString(" ")
+
     fun decode(input: String): String = ""
+}
+
+fun main(args: Array<String>) {
+    val a = "thequickbrownfoxjumpsoverthelazydog"
+    println(Atbash.encode(a))
 }
