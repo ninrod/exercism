80144846186cea9823ac7f5379882909cb1f5fe4
ninrod <filipe.silva@gmail.com>
2018-05-16 12:44:21 -0300

    atbash: refine solution

--- atbash-cipher/src/main/kotlin/Atbash.kt
+++ atbash-cipher/src/main/kotlin/Atbash.kt
@@ -1,21 +1,14 @@
 object Atbash {
-    val alphabet = "abcdefghijklmnopqrstuvwxyz"
-    val numbers = "123456789"
-    val e = alphabet + numbers
-    val er = alphabet.reversed() + numbers
-    var map: Map<Char, Char> = e.mapIndexed { i, c ->  c to er[i] }.toMap()
-    val reversed = map.entries.associateBy({ it.value }) { it.key }
-    val regex = """[${' '},.,${'!'},${'?'},${','}]""".toRegex()
-
-    fun encode(input: String): String = input
-            .replace(regex, "")
+    private val alphabet = ('a'..'z').toList().joinToString("")
+    private val numbers = (0..9).toList().joinToString("")
+    private val e = alphabet + numbers
+    private val er = alphabet.reversed() + numbers
+    private var map: Map<Char, Char> = e.mapIndexed { i, c ->  c to er[i] }.toMap()
+    private val reversedMap = map.entries.associateBy({ it.value }) { it.key }
+    private fun cifra(input: String, map: Map<Char,Char>): String = input
+            .filter { it.isLetterOrDigit() }
             .toLowerCase()
             .map { map[it] }.joinToString("")
-            .chunked(5)
-            .joinToString(" ")
-
-    fun decode(input: String): String = input
-            .replace(regex, "")
-            .toLowerCase()
-            .map { reversed[it] }.joinToString("")
+    fun encode(input: String): String = cifra(input, map).chunked(5).joinToString(" ")
+    fun decode(input: String): String = cifra(input, reversedMap)
 }
