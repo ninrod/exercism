db6b18d3c5abc7531a3ca419fcfc9f2affe722ef
ninrod <filipe.silva@gmail.com>
2018-05-17 12:05:28 -0300

    nth-prime: add incomplete implementation

--- nth-prime/src/main/kotlin/Prime.kt
+++ nth-prime/src/main/kotlin/Prime.kt
@@ -1,3 +1,24 @@
 object Prime {
-   fun nth(n: Int): Int = n
+    fun isPrime(n: Int): Boolean {
+        val a = n/2
+        (a downTo 2).forEach {
+            if (a % it == 0) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun nth(n: Int): Int {
+        var acc = 0
+        generateSequence(2, Int::inc).forEach {
+
+       }
+       return n
+   }
+}
+
+fun main(args: Array<String>) {
+    val prime = 14
+    println(Prime.isPrime(prime))
 }
