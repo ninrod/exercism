b553ce37b666dddc7f78403ff96c24e38368c719
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-05-18 17:44:04 -0300

    nth-prime: add working solution

--- nth-prime/src/main/kotlin/Prime.kt
+++ nth-prime/src/main/kotlin/Prime.kt
@@ -1,24 +1,14 @@
+fun Int.isPrime(): Boolean {
+    (this/2 downTo 2).forEach { if (this % it == 0) return false }
+    return true
+}
 object Prime {
-    fun isPrime(n: Int): Boolean {
-        val a = n/2
-        (a downTo 2).forEach {
-            if (a % it == 0) {
-                return true
-            }
-        }
-        return false
-    }
-
     fun nth(n: Int): Int {
+        require(n > 0) { "There is no zeroth prime." }
         var acc = 0
         generateSequence(2, Int::inc).forEach {
-
-       }
-       return n
-   }
-}
-
-fun main(args: Array<String>) {
-    val prime = 14
-    println(Prime.isPrime(prime))
+            if (it.isPrime()) if (++acc == n) return it
+        }
+        return 0
+    }
 }
