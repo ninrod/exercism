903ca8a19ce0b60d6ed8d3762676a233faf9bf2e
ninrod <filipe.silva@gmail.com>
2018-06-05 07:35:40 -0300

    remove cpp from toolbelt

--- cpp/hello-world/hello_world.cpp
+++
@@ -1,7 +0,0 @@
-#include "hello_world.h"
-
-namespace hello_world {
-  std::string hello() {
-    return "Hello, World!";
-  }
-}
--- cpp/hello-world/hello_world_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "hello_world.cpp"
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(test_hello)
-{
-    BOOST_REQUIRE_EQUAL("Hello, World!", hello_world::hello());
-}
