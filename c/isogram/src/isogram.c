e8baf2595cf18203845b2f0d3a01047dfff9bfa4
ninrod <filipe.silva@gmail.com>
2018-06-09 07:58:12 -0300

    move string.h to header

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include "isogram.h"
 
 bool is_isogram(const char phrase[]) {
--- c/isogram/src/isogram.h
+++ c/isogram/src/isogram.h
@@ -2,6 +2,7 @@
 #define ISOGRAM_H
 
 #include <stdbool.h>
+#include <string.h>
 
 bool is_isogram(const char phrase[]);
 
