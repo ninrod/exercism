004d80b984d864d7e5a835dac81219b81806ba15
ninrod <filipe.silva@gmail.com>
2018-06-09 16:04:52 -0300

    c.isogram: turn on other tests

--- c/isogram/test/test_isogram.c
+++ c/isogram/test/test_isogram.c
@@ -69,14 +69,14 @@ int main(void)
 
    RUN_TEST(test_filter);
    RUN_TEST(test_empty_string);
-   /* RUN_TEST(test_lower_case_only); */
-   /* RUN_TEST(test_duplicated_letter); */
-   /* RUN_TEST(test_longest_known_isogram); */
-   /* RUN_TEST(test_duplicated_letter_mixed_case); */
-   /* RUN_TEST(test_non_letter_char); */
-   /* RUN_TEST(test_duplicated_non_letter_char); */
-   /* RUN_TEST(test_multiple_whitespace); */
-   /* RUN_TEST(test_duplicated_letter_within_word); */
+   RUN_TEST(test_lower_case_only);
+   RUN_TEST(test_duplicated_letter);
+   RUN_TEST(test_longest_known_isogram);
+   RUN_TEST(test_duplicated_letter_mixed_case);
+   RUN_TEST(test_non_letter_char);
+   RUN_TEST(test_duplicated_non_letter_char);
+   RUN_TEST(test_multiple_whitespace);
+   RUN_TEST(test_duplicated_letter_within_word);
 
    UnityEnd();
    return 0;
