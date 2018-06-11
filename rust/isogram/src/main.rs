7edd658649953e0129a72547f4fdb0bccc7b055a
ninrod <filipe.silva@gmail.com>
2018-06-11 10:35:24 -0300

    rust.isogram: sorting is already working

--- rust/isogram/src/main.rs
+++ rust/isogram/src/main.rs
@@ -1,7 +1,18 @@
-extern crate isogram;
+// extern crate isogram;
+// use isogram::check;
 
-use isogram::check;
+use std::iter::FromIterator;
 
 fn main() {
-    println!("check = {}", check(""));
+    // println!("check = {}", check(""));
+
+    let wordy: String = "I am a hello world example".to_owned().to_lowercase();
+    let s_slice: &str = &wordy[..];
+
+    let mut chars: Vec<char> = s_slice.chars().filter(|c| c.is_alphabetic()).collect();
+    chars.sort_by(|a, b| a.cmp(b));
+
+    println!("test{:?}", chars);
+    let s: String = String::from_iter(chars);
+    println!("{}", s);
 }
