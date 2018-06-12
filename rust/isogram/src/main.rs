80a1c9ef3d9844ee962f0aa9e9c909d9ebe227b4
ninrod <filipe.silva@gmail.com>
2018-06-11 21:57:46 -0300

    rust.usogram: simplify

--- rust/isogram/src/main.rs
+++ rust/isogram/src/main.rs
@@ -6,13 +6,13 @@ use std::iter::FromIterator;
 fn main() {
     // println!("check = {}", check(""));
 
-    let wordy: String = "I am a hello world example".to_owned().to_lowercase();
-    let s_slice: &str = &wordy[..];
-
-    let mut chars: Vec<char> = s_slice.chars().filter(|c| c.is_alphabetic()).collect();
+    let mut chars = "filipe"
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .collect::<Vec<char>>();
     chars.sort_by(|a, b| a.cmp(b));
+    println!("test: {:?}", chars);
 
-    println!("test{:?}", chars);
     let s: String = String::from_iter(chars);
-    println!("{}", s);
+    println!("s = {}", s);
 }
