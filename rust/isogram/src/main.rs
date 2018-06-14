39cd91372a32972e3ff89f86f5842b339988f6ba
ninrod <filipe.silva@gmail.com>
2018-06-13 21:51:54 -0300

    rust.isogram: add working loop, finally

--- rust/isogram/src/main.rs
+++ rust/isogram/src/main.rs
@@ -2,6 +2,7 @@
 // use isogram::check;
 
 use std::iter::FromIterator;
+use std::cmp::Ordering;
 
 fn main() {
     // println!("check = {}", check(""));
@@ -15,6 +16,14 @@ fn main() {
 
     let s: String = String::from_iter(chars);
     for (i, e) in s.chars().skip(1).enumerate() {
-        println!("i = {}, j = {:?}", e, s.chars().nth(i).unwrap());
+        let c = s.chars().nth(i).unwrap();
+        println!("i = {}, j = {:?}", e, c);
+        match e.cmp(&c) {
+            Ordering::Equal => {
+                println!("found equal !");
+                break;
+            }
+            _ => println!("different. continuing"),
+        }
     }
 }
