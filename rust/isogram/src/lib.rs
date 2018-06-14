3cdfd334bd2f5f80aa8e949ea941921efbf5041c
ninrod <filipe.silva@gmail.com>
2018-06-14 08:43:45 -0300

    rust.isogram: move main impl to lib

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,9 +1,16 @@
+use std::cmp::Ordering;
+
 pub fn check(candidate: &str) -> bool {
     let mut chars = candidate
         .chars()
         .filter(|c| c.is_alphabetic())
         .collect::<Vec<char>>();
     chars.sort_by(|a, b| a.cmp(b));
-    println!("test: {:?}", chars);
-    unimplemented!("Is {} an isogram?", candidate);
+    for (i, e) in chars.iter().skip(1).enumerate() {
+        match e.cmp(chars.iter().nth(i).unwrap()) {
+            Ordering::Equal => return false,
+            _ => continue,
+        }
+    }
+    true
 }
