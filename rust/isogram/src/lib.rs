b16ccb8d9989568941812a38ca1642cc9cd995ea
ninrod <filipe.silva@gmail.com>
2018-06-14 08:50:33 -0300

    rust.isogram: fix lowercasing

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,10 +1,11 @@
 use std::cmp::Ordering;
 
 pub fn check(candidate: &str) -> bool {
-    let mut chars = candidate
+    let mut chars: Vec<char> = candidate
+        .to_lowercase()
         .chars()
         .filter(|c| c.is_alphabetic())
-        .collect::<Vec<char>>();
+        .collect();
     chars.sort_by(|a, b| a.cmp(b));
     for (i, e) in chars.iter().skip(1).enumerate() {
         match e.cmp(chars.iter().nth(i).unwrap()) {
