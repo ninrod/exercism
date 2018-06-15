df82c451751aac0aa9c7a552f7824f19a90428f9
ninrod <filipe.silva@gmail.com>
2018-06-14 23:37:33 -0300

    typescript.twofer: add exercise and solution

---
+++ typescript/two-fer/build/two-fer.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class TwoFer {
+    static twoFer(s = "you") {
+        return `One for ${s}, one for me.`;
+    }
+}
+exports.default = TwoFer;
+//# sourceMappingURL=two-fer.js.map
---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,7 @@
+class TwoFer {
+    static twoFer(s: String = "you") {
+        return `One for ${s}, one for me.`
+    }
+}
+
+export default TwoFer
