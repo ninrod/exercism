be6e0497a5877bf83e776f8494da88fcc83263dd
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-06-15 18:12:14 -0300

    add prop and setter

--- typescript/max-ts-course/app.ts
+++ typescript/max-ts-course/app.ts
@@ -25,8 +25,14 @@ console.log(`n = ${n}`);
 
 // const objects have mutable properties
 class Person {
+    myProp: string = "";
     constructor(public name: string, public age: number) { }
+
+    setProp(p: string) {
+        this.myProp = p;
+    }
 }
 const p: Person = new Person("filipe", 36)
 p.age = 37;
+p.setProp("lol")
 console.log(p);
