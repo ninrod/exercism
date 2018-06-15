0f095231195fec6dbfbc2e9053564f722d17e012
ninrod <filipe.silva@gmail.com>
2018-06-14 23:56:23 -0300

    elisp.two_fer: add exercise

---
+++ elisp/two-fer/two-fer.el
@@ -0,0 +1,15 @@
+;;; two-fer.el --- Two-fer Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun two-fer (args)
+  "docstring"
+  (interactive "P")
+  args
+  )
+
+
+(provide 'two-fer)
+;;; two-fer.el ends here
