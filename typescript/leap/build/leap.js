e534ab96785eedd3deb7d5205b2e0bfad38af7bf
ninrod <filipe.silva@gmail.com>
2018-06-14 23:41:25 -0300

    typescript.leap: add exercise

---
+++ typescript/leap/build/leap.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function isLeapYear(y) {
+    return y % 4 == 0;
+}
+exports.default = isLeapYear;
+//# sourceMappingURL=leap.js.map
---
+++ typescript/leap/leap.ts
@@ -0,0 +1,5 @@
+function isLeapYear(y: number) {
+    return y % 4 == 0;
+}
+
+export default isLeapYear
