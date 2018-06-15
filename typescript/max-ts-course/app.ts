e34dd80d0ab01e741a93627af51a4c7b403edb78
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-06-15 15:54:50 -0300

    add doc

--- typescript/max-ts-course/app.ts
+++ typescript/max-ts-course/app.ts
@@ -1,13 +1,17 @@
+// type aliasing
 type Complex = {
     num: number,
     a: string[]
 }
 
+// type aliasing usage
 let o: Complex = {
     num: 2,
     a: ["filipe", "silva"]
 }
 
+// destructuring demo
 let { a: [, f] } = o;
 
+// string interpolation
 console.log(`f = ${f}`);
