7774e78fe05aa319fb69c7d364b96ac937174373
ninrod <filipe.silva@gmail.com>
2018-06-18 11:54:08 -0300

    kotlin.leap: trying to write a divisible extension for Int

--- kotlin/leap/src/main/kotlin/Year.kt
+++ kotlin/leap/src/main/kotlin/Year.kt
@@ -1,3 +1,17 @@
+// on every year that is evenly divisible by 4
+//   except every year that is evenly divisible by 100
+//     unless the year is also evenly divisible by 400
+
+Int.divisible(d: Int): Boolean = this % d == 0
+
 data class Year (val year: Int) {
-    val isLeap: Boolean get() = true
+    val isLeap: Boolean get() {
+        val d4 = year.divisible(4)
+        if (! d4) return false
+
+        val d100 = year.divisible(100)
+        val d400 = year.divisible(400)
+
+        return d100 && !d400
+    }
 }
