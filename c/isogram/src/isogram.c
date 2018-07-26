03d75d6e7e0db61fa93407a8cc0beb248e240cff
ninrod <filipe.silva@gmail.com>
2018-07-25 21:53:39 -0300

    c-isogram: simplify array creation

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 
 bool is_isogram(const char phrase[]) {
-  int hm[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  int hm[26] = {0};
   while (*phrase != '\0') {
     if (isalpha(*phrase)) {
       int s = tolower(*phrase) - 'a';
