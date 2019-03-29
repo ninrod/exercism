d273ad7e8056057d299e8bd3d5da4bcc95eebd23
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-03-28 21:39:20 -0300

    refactor gigasecond

--- kotlin/gigasecond/src/test/kotlin/GigasecondTest.kt
+++ kotlin/gigasecond/src/test/kotlin/GigasecondTest.kt
@@ -1,59 +1,56 @@
-import org.junit.Test
+import org.amshove.kluent.*
+import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.Month
-import kotlin.test.assertEquals
 
 class GigasecondTest {
+    private fun Gigasecond.check(year: Int,
+                            month: Month,
+                            day: Int,
+                            hour: Int,
+                            minute: Int,
+                            second: Int) = with(this) {
+        date?.shouldBeInYear(year)
+        date?.shouldBeIn(month)
+        date?.dayOfMonth?.`should be equal to`(day)
+        date?.`should be in hour`(hour)
+        date?.`should be in minute`(minute)
+        date?.`should be in second`(second)
+    }
 
     @Test
     fun jtime() {
-        val d = LocalDate.of(2015,Month.MARCH, 27)
-        val g = Gigasecond(d)
-        val exp = LocalDateTime.of(2046 , Month.DECEMBER, 3, 1, 46, 40)
-        assertEquals(exp, g.date)
+        Gigasecond(LocalDate.of(2015,Month.MARCH, 27)).check(2046, Month.DECEMBER, 3, 1, 46, 40)
     }
 
     @Test
     fun atime() {
-        val d = LocalDate.of(1982,Month.JANUARY, 25)
-        val g = Gigasecond(d)
-        val exp = LocalDateTime.of(2013, Month.OCTOBER, 3, 1, 46, 40)
-        assertEquals(exp, g.date)
+        Gigasecond(LocalDate.of(1982,Month.JANUARY, 25)).check(2013, Month.OCTOBER, 3, 1, 46, 40)
     }
 
     @Test
     fun modernTime() {
-        val gigaSecond = Gigasecond(LocalDate.of(2011, Month.APRIL, 25))
-
-        assertEquals(LocalDateTime.of(2043, Month.JANUARY, 1, 1, 46, 40), gigaSecond.date)
+        Gigasecond(LocalDate.of(2011, Month.APRIL, 25)).check(2043, Month.JANUARY, 1, 1, 46, 40)
     }
 
     @Test
     fun afterEpochTime() {
-        val gigaSecond = Gigasecond(LocalDate.of(1977, Month.JUNE, 13))
-
-        assertEquals(LocalDateTime.of(2009, Month.FEBRUARY, 19, 1, 46, 40), gigaSecond.date)
+        Gigasecond(LocalDate.of(1977, Month.JUNE, 13)).check(2009, Month.FEBRUARY, 19, 1, 46, 40)
     }
 
     @Test
     fun beforeEpochTime() {
-        val gigaSecond = Gigasecond(LocalDate.of(1959, Month.JULY, 19))
-
-        assertEquals(LocalDateTime.of(1991, Month.MARCH, 27, 1, 46, 40), gigaSecond.date)
+        Gigasecond(LocalDate.of(1959, Month.JULY, 19)).check(1991, Month.MARCH, 27, 1, 46, 40)
     }
 
     @Test
     fun withFullTimeSpecified() {
-        val gigaSecond = Gigasecond(LocalDateTime.of(2015, Month.JANUARY, 24, 22, 0, 0))
-
-        assertEquals(LocalDateTime.of(2046, Month.OCTOBER, 2, 23, 46, 40), gigaSecond.date)
+        Gigasecond(LocalDateTime.of(2015, Month.JANUARY, 24, 22, 0, 0)).check(2046, Month.OCTOBER, 2, 23, 46, 40)
     }
 
     @Test
     fun withFullTimeSpecifiedAndDayRollover() {
-        val gigaSecond = Gigasecond(LocalDateTime.of(2015, Month.JANUARY, 24, 23, 59, 59))
-
-        assertEquals(LocalDateTime.of(2046, Month.OCTOBER, 3, 1, 46, 39), gigaSecond.date)
+        Gigasecond(LocalDateTime.of(2015, Month.JANUARY, 24, 23, 59, 59)).check(2046, Month.OCTOBER, 3, 1, 46, 39)
     }
 }
