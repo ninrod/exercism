882a3b904b72a9034730cbcd30b3d83808add7d7
ninrod <filipe.silva@gmail.com>
2019-04-08 08:59:48 -0300

    migrate largest-series-product

--- kotlin/largest-series-product/src/test/kotlin/SeriesTest.kt
+++ kotlin/largest-series-product/src/test/kotlin/SeriesTest.kt
@@ -1,96 +1,97 @@
-
-import org.junit.Test
-import kotlin.test.assertEquals
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class SeriesTest {
-
     @Test
     fun findsTheLargestProductIfSpanEqualsLength() {
-        assertEquals(18, Series("29").getLargestProduct(2))
+        18L `should be equal to`  Series("29").getLargestProduct(2)
     }
 
 
     @Test
     fun findsTheLargestProductOf2WithNumbersInOrder() {
-        assertEquals(72, Series("0123456789").getLargestProduct(2))
+        72L `should be equal to`  Series("0123456789").getLargestProduct(2)
     }
 
 
     @Test
     fun findsTheLargestProductOf2() {
-        assertEquals(48, Series("576802143").getLargestProduct(2))
+        48L `should be equal to`  Series("576802143").getLargestProduct(2)
     }
 
 
     @Test
     fun findsTheLargestProductOf3WithNumbersInOrder() {
-        assertEquals(504, Series("0123456789").getLargestProduct(3))
+        504L `should be equal to`  Series("0123456789").getLargestProduct(3)
     }
 
 
     @Test
     fun findsTheLargestProductOf3() {
-        assertEquals(270, Series("1027839564").getLargestProduct(3))
+        270L `should be equal to`  Series("1027839564").getLargestProduct(3)
     }
 
 
     @Test
     fun findsTheLargestProductOf5WithNumbersInOrder() {
-        assertEquals(15120, Series("0123456789").getLargestProduct(5))
+        15120L `should be equal to`  Series("0123456789").getLargestProduct(5)
     }
 
 
     @Test
     fun findsTheLargestProductWithinABigNumber() {
-        assertEquals(23520, Series("73167176531330624919225119674426574742355349194934").getLargestProduct(6))
+        23520L `should be equal to`  Series("73167176531330624919225119674426574742355349194934").getLargestProduct(6)
     }
 
 
     @Test
     fun reports0IfAllDigitsAre0() {
-        assertEquals(0, Series("0000").getLargestProduct(2))
+        0L `should be equal to`  Series("0000").getLargestProduct(2)
     }
 
 
     @Test
     fun reports0IfAllSpansInclude0() {
-        assertEquals(0, Series("99099").getLargestProduct(3))
+        0L `should be equal to`  Series("99099").getLargestProduct(3)
     }
-
-
-    @Test(expected = IllegalArgumentException::class)
-    fun rejectsSpanLongerThanStringLength() {
-        Series("123").getLargestProduct(4)
-    }
-
 
     @Test
     fun reports1ForEmptyStringAndEmptyProduct() {
-        assertEquals(1, Series("").getLargestProduct(0))
+        1L `should be equal to`  Series("").getLargestProduct(0)
     }
 
 
     @Test
     fun reports1ForNonEmptyStringAndEmptyProduct() {
-        assertEquals(1, Series("123").getLargestProduct(0))
+        1L `should be equal to`  Series("123").getLargestProduct(0)
     }
 
-
-    @Test(expected = IllegalArgumentException::class)
+    @Test
     fun rejectsEmptyStringAndNonZeroSpan() {
-        Series("").getLargestProduct(1)
+        assertThrows<java.lang.IllegalArgumentException>("should throw illegal argument") {
+            Series("").getLargestProduct(1)
+        }
     }
 
-
-    @Test(expected = IllegalArgumentException::class)
+    @Test
     fun rejectsInvalidCharacterInDigits() {
-        Series("1234a5")
+        assertThrows<java.lang.IllegalArgumentException> ("should throw illegal argument") {
+            Series("1234a5")
+        }
     }
 
-
-    @Test(expected = IllegalArgumentException::class)
+    @Test
     fun rejectsNegativeSpan() {
-        Series("12345").getLargestProduct(-1)
+        assertThrows<java.lang.IllegalArgumentException> ("should throw illegal argument") {
+            Series("12345").getLargestProduct(-1)
+        }
     }
 
+    @Test
+    fun rejectsSpanLongerThanStringLength() {
+        assertThrows<java.lang.IllegalArgumentException>("should throw illegal argument") {
+            Series("123").getLargestProduct(4)
+        }
+    }
 }
