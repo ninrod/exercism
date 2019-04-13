3931634a880945773a076434da6c2eec253f3970
ninrod <filipe.silva@gmail.com>
2019-04-13 09:07:59 -0300

    migrate sum-of-multiples

--- kotlin/sum-of-multiples/src/test/kotlin/SumOfMultiplesTest.kt
+++ kotlin/sum-of-multiples/src/test/kotlin/SumOfMultiplesTest.kt
@@ -1,71 +1,69 @@
-import org.junit.Test
-import kotlin.test.assertEquals
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
 
 class SumOfMultiplesTest {
-
     @Test
     fun `multiples of 3 or 5 up to 1`() {
-        assertEquals(0, SumOfMultiples.sum(setOf(3, 5), 1))
+        SumOfMultiples.sum(setOf(3, 5), 1) `should be equal to` 0
     }
 
     @Test
     fun `multiples of 3 or 5 up to 4`() {
-        assertEquals(3, SumOfMultiples.sum(setOf(3, 5), 4))
+        SumOfMultiples.sum(setOf(3, 5), 4) `should be equal to` 3
     }
 
     @Test
     fun `multiples of 3 up to 7`() {
-        assertEquals(9, SumOfMultiples.sum(setOf(3), 7))
+        SumOfMultiples.sum(setOf(3), 7) `should be equal to` 9
     }
 
     @Test
     fun `multiples of 3 or 5 up to 10`() {
-        assertEquals(23, SumOfMultiples.sum(setOf(3, 5), 10))
+        SumOfMultiples.sum(setOf(3, 5), 10) `should be equal to` 23
     }
 
     @Test
     fun `multiples of 3 or 5 up to 100`() {
-        assertEquals(2318, SumOfMultiples.sum(setOf(3, 5), 100))
+        SumOfMultiples.sum(setOf(3, 5), 100) `should be equal to` 2318
     }
 
     @Test
     fun `multiples of 3 or 5 up to 1000`() {
-        assertEquals(233168, SumOfMultiples.sum(setOf(3, 5), 1000))
+        SumOfMultiples.sum(setOf(3, 5), 1000) `should be equal to` 233168
     }
 
     @Test
     fun `multiples of 7, 13 or 17 up to 20`() {
-        assertEquals(51, SumOfMultiples.sum(setOf(7, 13, 17), 20))
+        SumOfMultiples.sum(setOf(7, 13, 17), 20) `should be equal to` 51
     }
 
     @Test
     fun `multiples of 4 or 6 up to 15`() {
-        assertEquals(30, SumOfMultiples.sum(setOf(4, 6), 15))
+        SumOfMultiples.sum(setOf(4, 6), 15) `should be equal to` 30
     }
 
     @Test
     fun `multiples of 5, 6 or 8 up to 150`() {
-        assertEquals(4419, SumOfMultiples.sum(setOf(5, 6, 8), 150))
+        SumOfMultiples.sum(setOf(5, 6, 8), 150) `should be equal to` 4419
     }
 
     @Test
     fun `multiples of 5 or 25 up to 51`() {
-        assertEquals(275, SumOfMultiples.sum(setOf(5, 25), 51))
+          SumOfMultiples.sum(setOf(5, 25), 51) `should be equal to` 275
     }
 
     @Test
     fun `multiples of 43 or 47 up to 10000`() {
-        assertEquals(2203160, SumOfMultiples.sum(setOf(43, 47), 10000))
+        SumOfMultiples.sum(setOf(43, 47), 10000) `should be equal to` 2203160
     }
 
     @Test
     fun `multiples of 1 up to 100`() {
-        assertEquals(4950, SumOfMultiples.sum(setOf(1), 100))
+        SumOfMultiples.sum(setOf(1), 100) `should be equal to` 4950
     }
 
     @Test
     fun `multiples of an empty set up to 10000`() {
-        assertEquals(0, SumOfMultiples.sum(emptySet(), 10000))
+        SumOfMultiples.sum(emptySet(), 10000) `should be equal to` 0
     }
-
 }
