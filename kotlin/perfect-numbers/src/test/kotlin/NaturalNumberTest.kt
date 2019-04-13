b341c6c5ec2e43be6881b47620f4729d1953584c
ninrod <filipe.silva@gmail.com>
2019-04-13 08:35:26 -0300

    migrate perfect numbers

--- kotlin/perfect-numbers/src/test/kotlin/NaturalNumberTest.kt
+++ kotlin/perfect-numbers/src/test/kotlin/NaturalNumberTest.kt
@@ -1,71 +1,75 @@
-import org.junit.Test
-
-import org.junit.Assert.assertEquals
+import org.amshove.kluent.`should be`
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.RuntimeException
 
 class NaturalNumberTest {
-
     @Test
     fun smallPerfectNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.PERFECT, classify(6))
+        Classification.PERFECT `should be`  classify(6)
     }
 
     @Test
     fun mediumPerfectNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.PERFECT, classify(28))
+        Classification.PERFECT `should be`  classify(28)
     }
 
     @Test
     fun largePerfectNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.PERFECT, classify(33550336))
+        Classification.PERFECT `should be`  classify(33550336)
     }
 
     @Test
     fun smallAbundantNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.ABUNDANT, classify(12))
+        Classification.ABUNDANT `should be`  classify(12)
     }
 
     @Test
     fun mediumAbundantNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.ABUNDANT, classify(30))
+        Classification.ABUNDANT `should be`  classify(30)
     }
 
     @Test
     fun largeAbundantNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.ABUNDANT, classify(33550335))
+        Classification.ABUNDANT `should be`  classify(33550335)
     }
 
     @Test
     fun smallestPrimeDeficientNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.DEFICIENT, classify(2))
+        Classification.DEFICIENT `should be`  classify(2)
     }
 
     @Test
     fun smallestNonPrimeDeficientNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.DEFICIENT, classify(4))
+        Classification.DEFICIENT `should be`  classify(4)
     }
 
     @Test
     fun mediumNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.DEFICIENT, classify(32))
+        Classification.DEFICIENT `should be`  classify(32)
     }
 
     @Test
     fun largeDeficientNumberIsClassifiedCorrectly() {
-        assertEquals(Classification.DEFICIENT, classify(33550337))
+        Classification.DEFICIENT `should be`  classify(33550337)
     }
 
     @Test
     fun edgeCaseWithNoFactorsOtherThanItselfIsClassifiedCorrectly() {
-        assertEquals(Classification.DEFICIENT, classify(1))
+        Classification.DEFICIENT `should be`  classify(1)
     }
 
-    @Test(expected = RuntimeException::class)
+    @Test
     fun zeroIsNotANaturalNumber() {
-        classify(0)
+        assertThrows<java.lang.RuntimeException> {
+            classify(0)
+        }
     }
 
-    @Test(expected = RuntimeException::class)
+    @Test
     fun negativeNumberIsNotANaturalNumber() {
-        classify(-1)
+        assertThrows<RuntimeException> {
+            classify(-1)
+        }
     }
 }
