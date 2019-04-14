07c3a59f295139233d915387be2cc9da8b4e3372
ninrod <filipe.silva@gmail.com>
2019-04-14 18:05:10 -0300

    solve word-count

--- kotlin/word-count/src/main/kotlin/WordCount.kt
+++ kotlin/word-count/src/main/kotlin/WordCount.kt
@@ -1,3 +1,23 @@
+fun String.sanitize(): String {
+    return this.replace("[,:.\n!&@\$%^]".toRegex(), " ")
+            .replace("\\s+".toRegex(), " ")
+            .trim()
+            .toLowerCase()
+}
+
+fun String.unquote(): String =
+        if (this.first() == '\'' && this.last() == '\'') this.substring(1 until this.length - 1) else this
+
 object WordCount {
-    fun phrase(s: String): Map<String, Int> = mapOf()
+    fun phrase(str: String): Map<String, Int> {
+        val s = str.sanitize()
+        val m = HashMap<String, Int>()
+        s.split(" ").map { it.unquote() }.forEach {
+            if (m.containsKey(it))
+                m[it] = (m[it]?: 0) + 1
+            else
+                m[it] = 1
+        }
+        return m
+    }
 }
