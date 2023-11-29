dbdfb72792123290fbf2af3a75d7c7c69f553eb8
ninrod <filipe.silva@gmail.com>
2023-11-28 22:49:03 -0300

    more

--- js/primes/index.js
+++ js/primes/index.js
@@ -1,16 +1,11 @@
-function is_prime(p) {
-  for(let i=2; i<p/2;i++)
-    if (p % i === 0)
-      return false
-  return true
-}
-
-function n_first(n) {
+((n) => {
   a=[]
   for(let i=1 ;i <= n;i++)
     a.push(i)
   return a
-}
-
-n_first(1000).filter(p => is_prime(p)).forEach(e => console.log(e))
-
+})(1000).filter(p => {
+  for(let i=2; i<p/2;i++)
+    if (p % i === 0)
+      return false
+  return true
+}).forEach(e => console.log(e))
